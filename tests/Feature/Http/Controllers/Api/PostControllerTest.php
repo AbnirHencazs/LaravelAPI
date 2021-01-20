1fb6a4e1e75b72589e4b53efb659b512cabfa80e
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-20 12:25:51 -0600

    Prueba para store del PostContrller con 4 assertions

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,36 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
+    use RefreshDatabase; //Como se va a construir datos y se buscara en la base de datos, use esta clase
     /**
-     * A basic feature test example.
+     * Test for the store method on PostController
      *
      * @return void
      */
-    public function test_example()
+    public function test_store()
     {
-        $response = $this->get('/');
+        /**
+        *Estamos simulando que cualquier aplicación que se conecte a la ruta indicada e intente hacer un POST
+        *para guardar los datos dentro del array para comprobar que se guardan correctamente */
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'El post de prueba'
+        ]);
 
-        $response->assertStatus(200);
+        /**
+         * La primera confirmación (assert) será que nuestro sistema retorne, en una estructura Json,
+         * los datos dentro del array
+         * 
+         * La segunda comprobación será comprobar que existe el objeto 'title'
+         * 
+         * La tercera comprobación será la respuesta HTTP que esperamos obtener cuando se guarde correctamente
+         */
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'udpated_at'])
+                ->assertJson(['title' => 'El post de prueba'])
+                ->asserStatus(200);
+        /**
+         * Confirmamos que en efecto existe el dato publicado en la base de datos
+         */
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+
     }
 }
